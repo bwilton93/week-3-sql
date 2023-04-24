a8f47a1a9818132ecf10c8643287804bce76e42e
Barney <B.Wilton1993@gmail.com>
2023-04-24 18:16:55 +0100

    rspec test for creating entries

--- phase_two/music_library/lib/album_repository.rb
+++ phase_two/music_library/lib/album_repository.rb
@@ -22,16 +22,50 @@ class AlbumRepository
   end
 
   def find_by_id(id)
-    sql = "SELECT * FROM albums WHERE id = #{id}"
+    sql = "SELECT * FROM albums WHERE id = #{id};"
     result = DatabaseConnection.exec_params(sql, [])
-
+    
     album = Album.new
     
     album.id = result.first["id"]
     album.title = result.first["title"]
     album.release_year = result.first["release_year"]
-    album.artist_id = result.first["artist_id".to_i]
+    album.artist_id = result.first["artist_id"]
+    
+    return album
+  end
+  
+  def find_by_name(name)
+    sql = "SELECT * FROM albums WHERE title = '#{name}';"
+    result = DatabaseConnection.exec_params(sql, [])
+
+    album = Album.new
+
+    album.id = result.first["id"]
+    album.title = result.first["title"]
+    album.release_year = result.first["release_year"]
+    album.artist_id = result.first["artist_id"]
 
     return album
+  end
+
+  def find_by_release_year(year)
+    sql = "SELECT * FROM albums WHERE release_year = #{year};"
+    result = DatabaseConnection.exec_params(sql, [])
+
+    album = Album.new
+
+    album.id = result.first["id"]
+    album.title = result.first["title"]
+    album.release_year = result.first["release_year"]
+    album.artist_id = result.first["artist_id"]
+
+    return album
+  end
+
+  def create(title, release_year, artist_id = 0)
+    sql = "INSERT INTO albums (title, release_year, artist_id) VALUES ('#{title}', #{release_year}, #{artist_id});"
+
+    DatabaseConnection.exec_params(sql, [])
   end
 end
--- phase_two/music_library/spec/album_repository_spec.rb
+++ phase_two/music_library/spec/album_repository_spec.rb
@@ -7,9 +7,9 @@ def reset_albums_table
 end
   
 describe AlbumRepository do
-  before(:each) do 
-    reset_albums_table
-  end
+  # before(:each) do 
+  #   reset_albums_table
+  # end
 
   it "fetches all albums" do
     repo = AlbumRepository.new
@@ -34,4 +34,51 @@ describe AlbumRepository do
     album = repo.find_by_id(1)
     expect(album.title).to eq "Album 1"
   end
+
+  it "fetches album when passed album title" do
+    repo = AlbumRepository.new
+
+    album = repo.find_by_name("Album 1")
+    expect(album.id).to eq "1"
+  end
+  
+  it "fetches album when passed release year" do
+    repo = AlbumRepository.new
+  
+    album = repo.find_by_release_year(2022)
+    expect(album.id).to eq "1"
+  end
+
+  it "can create a new album" do
+    repo = AlbumRepository.new
+
+    title = "Album 3"
+    release_year = 2023
+
+    repo.create(title, release_year)
+    
+    albums = repo.all
+    expect(albums.length).to eq 3
+    expect(albums[-1].id).to eq "3"
+    expect(albums[-1].title).to eq "Album 3"
+    expect(albums[-1].release_year).to eq "2023"
+    expect(albums[-1].artist_id).to eq "0"
+  end
+end
+
+def create(title, release_year, artist_id)
+# INSERT INTO albums
+# (title, release_year, artist_id)
+# VALUES(name, release_year, artist_id)
+# returns nothing
+end
+
+def update_release_year(album, year)
+# UPDATE albums SET release_year = year  WHERE id = album.id
+# returns nothing
+end
+
+def delete(album)
+# DELETE FROM albums WHERE id = album.id
+# returns nothing
 end
