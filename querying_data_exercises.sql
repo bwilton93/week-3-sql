70ccec72ef0a8029494bdac4dd9bbb4a922aded2
Barney <B.Wilton1993@gmail.com>
2023-04-24 11:47:09 +0100

    Crafted alternate solution for challenge based on querying artist ID from the artist table

--- querying_data_exercises.sql
+++ querying_data_exercises.sql
@@ -6,3 +6,8 @@ SELECT release_year FROM albums WHERE title = 'Bossanova';
 
 -- Find the titles of the albums released by 'Pixies' and released between 1980 and 1990 (inclusive).
 SELECT title FROM albums WHERE artist_id = 1 AND release_year >= 1980 AND release_year <= 1990;
+-- Alternative solution
+SELECT title FROM albums 
+  WHERE release_year >= 1980 AND release_year <= 1990 
+  AND artist_id IN 
+    (SELECT id FROM artists WHERE name = 'Pixies');
