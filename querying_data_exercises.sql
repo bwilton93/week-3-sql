3f76eb861b5bac2703f2ddb91454a03015fd1909
Barney <B.Wilton1993@gmail.com>
2023-04-24 11:39:44 +0100

    Finished the querying data excercises and challenge

---
+++ querying_data_exercises.sql
@@ -0,0 +1,8 @@
+-- Run a SELECT query to list values for the column release_year from the albums table.
+SELECT release_year FROM albums;
+
+-- Run a filtered SELECT query to list only the release_year of the album with title 'Bossanova'.
+SELECT release_year FROM albums WHERE title = 'Bossanova';
+
+-- Find the titles of the albums released by 'Pixies' and released between 1980 and 1990 (inclusive).
+SELECT title FROM albums WHERE artist_id = 1 AND release_year >= 1980 AND release_year <= 1990;
