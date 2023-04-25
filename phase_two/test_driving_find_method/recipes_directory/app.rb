52778b57c6670ed0fc457bbe50752b24b5c3b0c8
Barney <B.Wilton1993@gmail.com>
2023-04-25 12:13:59 +0100

    Wrote app.rb to output list of all recipes

--- phase_two/test_driving_find_method/recipes_directory/app.rb
+++ phase_two/test_driving_find_method/recipes_directory/app.rb
@@ -1,3 +1,15 @@
 require_relative 'lib/database_connection'
+require_relative 'lib/recipes_directory'
 
 DatabaseConnection.connect('recipes_directory')
+
+recipes_directory = RecipesDirectory.new
+
+recipes_directory.all.each do |recipe|
+  puts "#{recipe.id}. #{recipe.name.ljust(30)} - Cooking time: #{recipe.cooking_time.to_s.ljust(3)} - Rating: #{recipe.rating}"
+end
+
+
+p "outputting just the third entry on it's own:"
+recipe = recipes_directory.find_by_id(3)
+puts "#{recipe.id}. #{recipe.name.ljust(30)} - Cooking time: #{recipe.cooking_time.to_s.ljust(3)} - Rating: #{recipe.rating}"
