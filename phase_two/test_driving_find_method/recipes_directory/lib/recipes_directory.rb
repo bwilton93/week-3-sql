5c73256d397e67a2f220786e084c9caf9bc3ffa4
Barney <B.Wilton1993@gmail.com>
2023-04-25 12:07:45 +0100

    wrote test for find_by_id and then wrote code to pass test

--- phase_two/test_driving_find_method/recipes_directory/lib/recipes_directory.rb
+++ phase_two/test_driving_find_method/recipes_directory/lib/recipes_directory.rb
@@ -10,7 +10,7 @@ class RecipesDirectory
     results.each do |result|
       recipe = Recipe.new
 
-      recipe.id = result['id']
+      recipe.id = result['id'].to_i
       recipe.name = result['name']
       recipe.cooking_time = result['cooking_time'].to_i
       recipe.rating = result['rating'].to_i
@@ -19,5 +19,19 @@ class RecipesDirectory
     end
 
     return recipes
+  end
+
+  def find_by_id(id)
+    sql = "SELECT * FROM recipes WHERE id = $1;"
+    result = DatabaseConnection.exec_params(sql, [id])
+
+    recipe = Recipe.new
+
+    recipe.id = result.first['id'].to_i
+    recipe.name = result.first['name']
+    recipe.cooking_time = result.first['cooking_time'].to_i
+    recipe.rating = result.first['rating'].to_i
+
+    return recipe
   end
 end
--- phase_two/test_driving_find_method/recipes_directory/spec/recipes_directory_spec.rb
+++ phase_two/test_driving_find_method/recipes_directory/spec/recipes_directory_spec.rb
@@ -17,14 +17,25 @@ describe RecipesDirectory do
     recipes = repo.all
     expect(recipes.length).to eq 5
     
-    expect(recipes.first.id).to eq "1"
+    expect(recipes.first.id).to eq 1
     expect(recipes.first.name).to eq "Pepperoni Pizza"
     expect(recipes.first.cooking_time).to eq 10
     expect(recipes.first.rating).to eq 5
   
-    expect(recipes.last.id).to eq "5"
+    expect(recipes.last.id).to eq 5
     expect(recipes.last.name).to eq "Microwave Meal"
     expect(recipes.last.cooking_time).to eq 4
     expect(recipes.last.rating).to eq 2
+  end
+
+  it "fetches a single recipe by ID" do
+    repo = RecipesDirectory.new
+
+    recipe = repo.find_by_id(3)
+
+    expect(recipe.id).to eq 3
+    expect(recipe.name).to eq "Choriza and Broccoli Pasta"
+    expect(recipe.cooking_time).to eq 15
+    expect(recipe.rating).to eq 5
   end
 end
