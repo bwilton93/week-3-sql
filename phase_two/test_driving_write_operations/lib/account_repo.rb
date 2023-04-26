5abebac764b17d50ef41fa4e84d6e9538f50438a
Barney <B.Wilton1993@gmail.com>
2023-04-26 15:35:08 +0100

    Wrote test for #find method and wrote test for #create method

--- phase_two/test_driving_write_operations/lib/account_repo.rb
+++ phase_two/test_driving_write_operations/lib/account_repo.rb
@@ -14,7 +14,25 @@ class AccountRepo
       account.username = record["username"]
       accounts << account
     end
-
+    
     return accounts
+  end
+  
+  def find(id)
+    sql = "SELECT * FROM user_accounts WHERE id = $1;"
+    result = DatabaseConnection.exec_params(sql, [id]).first
+
+    account = Account.new
+    account.id = result["id"]
+    account.email_address = result["email_address"]
+    account.username = result["username"]
+
+    return account
+  end
+
+  def create
+  end
+
+  def delete
   end
 end
