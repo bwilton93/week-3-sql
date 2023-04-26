e3686c6fefda1e57a3c5fee5823cb806b98beaba
Evan Thomas <evanthomas0292@gmail.com>
2023-04-26 15:16:23 +0100

    Write implementation of AccountRepo#all

--- phase_two/test_driving_write_operations/lib/account_repo.rb
+++ phase_two/test_driving_write_operations/lib/account_repo.rb
@@ -1,2 +1,20 @@
+require "account"
+
 class AccountRepo
+  def all
+    accounts = []
+
+    sql = "SELECT * FROM user_accounts;"
+    records = DatabaseConnection.exec_params(sql, [])
+
+    records.each do |record|
+      account = Account.new
+      account.id = record["id"]
+      account.email_address = record["email_address"]
+      account.username = record["username"]
+      accounts << account
+    end
+
+    return accounts
+  end
 end
