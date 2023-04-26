a53a9b015748b2fb167acf7ac1df4fc47ea45d16
Evan Thomas <evanthomas0292@gmail.com>
2023-04-26 15:10:51 +0100

    Write a test for AccountRepo#all

--- phase_two/test_driving_write_operations/spec/account_repo_spec.rb
+++ phase_two/test_driving_write_operations/spec/account_repo_spec.rb
@@ -12,4 +12,20 @@ describe AccountRepo do
     reset_tables
   end
 
+  describe "#all" do
+    it "returns all records from the accounts table" do
+      repo = AccountRepo.new
+      accounts = repo.all
+      
+      expect(accounts.length).to eq 2
+      expect(accounts.first.id).to eq "1"
+      expect(accounts.first.email_address).to eq "Email 1"
+      expect(accounts.first.username).to eq "User 1"
+
+      expect(accounts.last.id).to eq "2"
+      expect(accounts.last.email_address).to eq "Email 2"
+      expect(accounts.last.username).to eq "User 2"
+    end
+  end
+
 end
