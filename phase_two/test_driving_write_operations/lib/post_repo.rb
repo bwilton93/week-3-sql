bcab8a81234d0eb1d56499b241904b76707ad0d5
Barney <B.Wilton1993@gmail.com>
2023-04-26 16:11:36 +0100

    Wrote PostRepo#all and PostRepo#find method tests and code to satisfy

--- phase_two/test_driving_write_operations/lib/post_repo.rb
+++ phase_two/test_driving_write_operations/lib/post_repo.rb
@@ -1,2 +1,46 @@
 class PostRepo
+  def all
+    posts = []
+
+    sql = "SELECT * FROM posts;"
+    records = DatabaseConnection.exec_params(sql, [])
+
+    records.each do |record|
+      post = Post.new
+
+      post.id = record['id']
+      post.title = record['title']
+      post.content = record['content']
+      post.number_of_views = record['number_of_views']
+      post.user_account_id = record['user_account_id']
+      
+      posts << post
+    end
+    
+    return posts
+  end
+  
+  def find(id)
+    sql = "SELECT * FROM posts WHERE id = $1;"
+    
+    record = DatabaseConnection.exec_params(sql, [id]).first
+    
+    post = Post.new
+    post.id = record['id']
+    post.title = record['title']
+    post.content = record['content']
+    post.number_of_views = record['number_of_views']
+    post.user_account_id = record['user_account_id']
+
+    return post
+  end
+
+  def create
+  end
+
+  def delete
+  end
+
+  def update
+  end
 end
--- phase_two/test_driving_write_operations/spec/post_repo_spec.rb
+++ phase_two/test_driving_write_operations/spec/post_repo_spec.rb
@@ -12,4 +12,37 @@ describe PostRepo do
     reset_tables
   end
 
+  describe "#all" do
+    it "returns all records from the posts table" do
+      repo = PostRepo.new
+      posts = repo.all
+      
+      expect(posts.length).to eq 1
+      expect(posts.first.id).to eq "1"
+      expect(posts.first.title).to eq "Post 1"
+      expect(posts.first.content).to eq "Some content"
+      expect(posts.first.number_of_views).to eq "2"
+      expect(posts.first.user_account_id).to eq "1"
+    end
+  end
+  
+  describe "#find" do
+    it "returns a single post from the posts table" do
+      repo = PostRepo.new
+      
+      post = repo.find(1)
+
+      expect(post.id).to eq "1"
+      expect(post.title).to eq "Post 1"
+      expect(post.content).to eq "Some content"
+      expect(post.number_of_views).to eq "2"
+      expect(post.user_account_id).to eq "1"
+    end
+  end
+
+  describe "#create" do
+    it "creates a new post in the post table" do
+      
+    end
+  end
 end
