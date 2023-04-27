a217936f6a61a52c6597f78798c3187e8bfaff68
Barney <B.Wilton1993@gmail.com>
2023-04-27 15:48:49 +0100

    Crafted SQL statement

--- phase_three/blog/lib/posts_repo.rb
+++ phase_three/blog/lib/posts_repo.rb
@@ -1,5 +1,14 @@
 class PostsRepo
   def find_with_comments(id)
-
+    sql = 'SELECT posts.id,
+                posts.title,
+                posts.content,
+                comments.id AS comment_id,
+                comments.content AS comment_content,
+                comments.author_name 
+            FROM posts
+              JOIN comments
+              ON comments.post_id = posts.id
+            WHERE posts.id = $1;'
   end
 end
