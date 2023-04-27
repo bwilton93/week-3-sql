455b179f609c08371786a84662e6e758c0114b40
Barney <B.Wilton1993@gmail.com>
2023-04-27 15:57:08 +0100

    Started working on output for posts & comments

--- phase_three/blog/app.rb
+++ phase_three/blog/app.rb
@@ -6,3 +6,8 @@ DatabaseConnection.connect('blogs_database')
 post_repo = PostsRepo.new
 post = post_repo.find_with_comments(1)
     
+puts "-" * 25
+puts "#{post.title} - #{post.content}"
+puts "-" * 25
+puts "Comments"
+puts "-" * 25
