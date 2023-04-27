cf4d1bbf8cdaee99974fc542755273ef83afb657
Barney <B.Wilton1993@gmail.com>
2023-04-27 12:11:20 +0100

    Challenge completed, all nina simone albums after 1975'

--- phase_three/sql_joins/exercises/exercises.sql
+++ phase_three/sql_joins/exercises/exercises.sql
@@ -14,3 +14,11 @@ SELECT albums.id,
     JOIN artists
     ON albums.artist_id = artists.id
   WHERE artists.name = 'Pixies' AND albums.release_year = 1988;
+
+-- #3 CHALLENGE: Find the id and title of all albums from Nina Simone released after 1975.
+SELECT albums.id,
+      albums.title
+  FROM albums
+    JOIN artists
+    ON albums.artist_id = artists.id
+  WHERE artists.name = 'Nina Simone' AND albums.release_year > 1975;
