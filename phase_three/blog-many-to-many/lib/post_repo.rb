6bd8b080813d21b4b390b34d521f16e6a8a251cb
Barney <B.Wilton1993@gmail.com>
2023-04-28 11:54:56 +0100

    wrote find_by_tag method for PostRepo class

--- phase_three/blog-many-to-many/lib/post_repo.rb
+++ phase_three/blog-many-to-many/lib/post_repo.rb
@@ -1,8 +1,26 @@
-class PostRepo
+require_relative './post'
 
-  sql = "SELECT posts.id, posts.title
-          FROM posts
-          JOIN posts_tags ON posts_tags.post_id = posts.id
-          JOIN tags ON posts_tags.tag_id = tags.id
-          WHERE tags.name = 'coding';"
+class PostRepo
+  def find_by_tag(name)
+    posts = []
+
+    sql = 'SELECT posts.id, posts.title
+            FROM posts
+            JOIN posts_tags ON posts_tags.post_id = posts.id
+            JOIN tags ON posts_tags.tag_id = tags.id
+            WHERE tags.name = $1;'
+    
+    results = DatabaseConnection.exec_params(sql, [name])
+
+    results.each do |result|
+      post = Post.new
+
+      post.id = result['id']
+      post.title = result['title']
+
+      posts << post
+    end
+
+    return posts
+  end
 end
